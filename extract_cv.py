33958f687a3b7a83c31ce4857c605b4fd06e5a09
Alim H <alimh@Alims-MacBook-Air.local>
2026-01-23 12:43:10 +0100

    Cleanup: Remove non-essential files from repository

--- extract_cv.py
+++
@@ -1,9 +0,0 @@
-from pypdf import PdfReader
-
-reader = PdfReader("CV_AlimHasanov.pdf")
-text = ""
-for page in reader.pages:
-    text += page.extract_text() + "\n"
-
-with open("cv_extracted.txt", "w") as f:
-    f.write(text)
